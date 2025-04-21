d074ec7034afe9e3b4e718ce2df0c3289c681682
David Damasceno <damasceno.d@hotmail.com>
2025-04-21 08:21:52 -0300

    2

--- app.go
+++ app.go
@@ -14,26 +14,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, `
-		<!DOCTYPE html>
-		<html>
-		<head>
-			<style>
-				body {
-					background-color: green;
-					color: white;
-					font-family: Arial, sans-serif;
-					text-align: center;
-					padding-top: 50px;
-				}
-			</style>
-		</head>
-		<body>
-			<h1>This request was processed by host: %s</h1>
-			<p> - V1</p>
-		</body>
-		</html>
-		`, hostname)
+	fmt.Fprintf(w, "<h1>This request was processed by host: %s</h1> - V2", hostname)
 }
 
 func main() {
